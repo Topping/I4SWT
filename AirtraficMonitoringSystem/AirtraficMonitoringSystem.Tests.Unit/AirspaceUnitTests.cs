14e38e9b79f8023e07a8f65e6479c0ca74f66b35
Nikolai Topping <nikolai.topping@gmail.com>
2017-04-20 09:04:19 +0200

    Test af en test

---
+++ AirtraficMonitoringSystem/AirtraficMonitoringSystem.Tests.Unit/AirspaceUnitTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AirtraficMonitoringSystem.Tests.Unit {
+    [TestFixture]
+    public class AirspaceUnitTests {
+        [Test]
+        public void WinTest() {
+            Assert.That(true,Is.True);
+        }
+    }
+}
